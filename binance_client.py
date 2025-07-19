412635eb9bb2cc5d54e8c0c7ecf26daf40ad7045
Mario Soberanis <elmaso@gmail.com>
2025-07-18 18:34:00 -0700

    clean

--- binance_client.py
+++ binance_client.py
@@ -1,15 +1,13 @@
 from binance.client import Client
 from pathlib import Path
-import json
+import os
 
 def get_binance_client() -> Client:
-    CDP_KEY_FILE = Path("cdp_api_key.json")
-    if not CDP_KEY_FILE.exists():
-        raise FileNotFoundError("cdp_api_key.json not found")
-    with open(CDP_KEY_FILE) as f:
-        creds = json.load(f)
-    api_key = creds.get("apiKey")
-    api_secret = creds.get("secretKey")
+    api_key = os.getenv("BINANCE_API_KEY")
+    api_secret = os.getenv("BINANCE_API_SECRET")
+
     if not api_key or not api_secret:
-        raise ValueError("API key or secret missing in cdp_api_key.json")
+        raise ValueError("API key or secret missing in environment variables")
+
+    
     return Client(api_key, api_secret) 
